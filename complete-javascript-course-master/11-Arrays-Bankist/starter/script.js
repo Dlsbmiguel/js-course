7c33af9c467e9a0fc171f1ce9139f221a2652aa9
Dlsmiuel <38094761+Dlsmiuel@users.noreply.github.com>
2021-11-15 18:34:09 -0400

    CC#3 Arrays

--- complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
+++ complete-javascript-course-master/11-Arrays-Bankist/starter/script.js
@@ -374,3 +374,14 @@ const totalDepositUSD = movements
   .reduce((acc, mov) => acc + mov, 0);
 console.log(totalDepositUSD);
 */
+
+/////////////////////////////////////////////////////////
+// Coding Challenge #3
+
+const calcAverageHumanAge = ages =>
+  ages
+    .map(age => (age <= 2 ? 2 * age : 16 + age * 4))
+    .filter(age => age >= 19)
+    .reduce((acc, age, i, arr) => (acc + age) / arr.length, 0);
+
+console.log(calcAverageHumanAge([5, 2, 4, 1, 15, 8, 3]));
