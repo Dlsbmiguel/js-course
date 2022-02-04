c745953ea891c3a6ea5df29bb0b894a846bfcadd
Dlsmiuel <38094761+Dlsmiuel@users.noreply.github.com>
2022-02-04 15:33:30 -0400

    seccion 18 completa
    ultima seccion de codigo forkify

---
+++ complete-javascript-course-master/18-forkify/starter/src/js/config.js
@@ -0,0 +1,5 @@
+export const API_URL = 'https://forkify-api.herokuapp.com/api/v2/recipes/';
+export const TIMEOUT_SEC = 10;
+export const RES_PER_PAGE = 10;
+export const KEY = 'fb867a98-5221-4a4c-8798-2d008d4f6ac3';
+export const MODAL_CLOSE_SEC = 2.5;
